2f50e88898599c0b6ecb39e99ace2d6836030de4
platinumpizza29 <kbelgi1@gmail.com>
2024-09-25 23:14:56 +0100

    Info Changes

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -4,39 +4,39 @@ import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
 
 const About = () => {
-    return (
-        <section
-            className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56"
-            id="about"
-        >
-            <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
-                <AnimatedTitle
-                    text={"Mudunuri bhaskara karthikeya varma."}
-                    className={
-                        "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
-                    }
-                    wordSpace={"mr-[14px]"}
-                    charSpace={"mr-[0.001em]"}
-                />
+  return (
+    <section
+      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pb-36 pt-16 md:pb-44 md:pt-20 lg:pb-56 lg:pt-20"
+      id="about"
+    >
+      <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
+        <AnimatedTitle
+          text={"Keyur Atul Bilgi."}
+          className={
+            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+          }
+          wordSpace={"mr-[14px]"}
+          charSpace={"mr-[0.001em]"}
+        />
 
-                <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
-                    <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
-                        <AnimatedBody text="pursuing my B.tech in woxen university. I want to learn more about Front-end & Full Stack Development and i have a good command in javascript, Next.js, react.js, HTML&CSS. I believe in trying out new things and picking up challenges as it is always great to have knowledge in variety of subjects and have various experiences to share. im more interested in developing my weaknesses..." />
+        <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
+          <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
+            <AnimatedBody text="I am a Web Developer with a demonstrated history of building scalable and user-friendly web applications using React.js and React Native. I have strong expertise in front-end technologies like HTML, CSS, ECMAScript, and TypeScript, with a focus on integrating front-end applications with RESTful APIs and optimizing performance. I am always eager to explore new technologies and take on challenges, as I believe that continuous learning and diverse experiences are crucial for growth. I am particularly interested in enhancing my skills in backend development and expanding my knowledge across the full development stack, which will allow me to contribute more holistically to future projects." />
 
-                        <AnimatedBody
-                            delay={0.1}
-                            text="Whether I’m designing a sleek user interface or coding a complex application, I’m always striving to create something unique and innovative. I love experimenting with new technologies and staying up-to-date with the latest trends in the tech world."
-                        />
+            <AnimatedBody
+              delay={0.1}
+              text="Whether I’m designing a sleek user interface or coding a complex application, I’m always striving to create something unique and innovative. I love experimenting with new technologies and staying up-to-date with the latest trends in the tech world."
+            />
 
-                        <AnimatedBody
-                            delay={0.2}
-                            text="Right now, I’m working on some exciting projects that I can’t wait to share with you. But I’m always open to new opportunities and collaborations."
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+            <AnimatedBody
+              delay={0.2}
+              text="Right now, I’m working on some exciting projects that I can’t wait to share with you. But I’m always open to new opportunities and collaborations."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
